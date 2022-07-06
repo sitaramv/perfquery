e1e8eaae188b42df8d47596b3f7b894702479471
sitaramv <sitaram.vemulapalli@couchbase.com>
2022-07-06 16:59:28 -0700

    update

--- mt.py
+++ mt.py
@@ -18,7 +18,7 @@ cfg = {
        "loaddata": True,                      # control bucket/scope/collection/index/data drop/creation/load
        "execute": False,                      # control execute queries
        "nthreads" : 85,                       # max number of client threads (might lowered by load setting)
-       "host": 'http://ec2-35-80-34-7.us-west-2.compute.amazonaws.com',         # querynode host ip
+       "host": 'http://172.23.97.79',         # querynode host ip
        "datareplicas": 2,                     # data replica setting
        "indexreplicas": 1,                    # data ndex replicas setting
        "memory": 24576,                       # datanode memory  (divided by nbuckets)
@@ -29,12 +29,15 @@ cfg = {
        "nindexes": 1,                         # number of indexes per collection
        "naindexes": 1,                        # number of array indexes per collection
        "workload" : "complex",                # workload type (see workloads)
-       "load":"90",                           # load percent (see loads)
+       "load":"90",                          # load percent (see loads)
        "batchsize": 100,                      # batchsize (i.e. qualified rows per query)
        "qualifiedbatches": 1,                 # number of batches (to increase qualified rows per query)
        "duration": 10,                        # execution duration in seconds
+       "ncores": 16,                          # number of query service cores
        "indexfile": "index.txt",              # index statements
        "workloadfile": "workload",            # workload statements
+       "backupdir":"/tmp/backups",            # backup dir
+       "usebackup":True,                      # use backup/restore vs load_data
        "indexes"  : [ "CREATE INDEX ix0 IF NOT EXISTS ON col0 (c0, f115) WITH {'defer_build': true, 'num_replica': indexreplicas }",
                       "CREATE INDEX ix1 IF NOT EXISTS ON col0 (c1, f115) WITH {'defer_build': true, 'num_replica': indexreplicas }",
                       "CREATE INDEX ix2 IF NOT EXISTS ON col0 (c2, f115) WITH {'defer_build': true, 'num_replica': indexreplicas }",
@@ -68,7 +71,7 @@ cfg = {
 #             "querytenantweight": {"free":1, "light":2, "moderate": 6, "overheavy": 12, "superheavy": 24}, # default no of collections 1, 2, 6, 12, 24
               "50":{"free": 5, "light":8, "moderate":5, "overheavy":1, "superheavy":1 },
               "90":{"free": 0, "light":5, "moderate":10, "overheavy":4, "superheavy":1 },
-              "100": {"free": 0, "light":0, "moderate":2, "overheavy":0, "superheavy":0 }
+              "100": {"free": 1, "light":1, "moderate":2, "overheavy":0, "superheavy":0 }
                }
       }
 
@@ -209,18 +212,36 @@ def load_data(conn, workload):
     for b in sorted(workload.keys()):
         bv = workload[b]
         create_javascript_udf(b)
+        backupcollection = ""
         for sc in range(0,len(bv["scopes"])) :
             sv = bv["scopes"][sc]
             create_udf(conn, b, sv["bc"]+"."+sv["name"])
             for cc in range(0,len(sv["collections"])) :
                 cv = sv["collections"][cc]
-                cmd = "./load_data --host " + host + " --username Administrator --password password "
-                cmd += " --batches " + str(cv["batches"]) + " --batch-size " + str(cv["batchsize"])
-                cmd += " --bucket " + b + " --scope " + sv["name"] + " --collection " + cv["name"]
-                systemcmd(cmd)
+                if not cfg["usebackup"] or bv["type"] == "free" or backupcollection == "" :
+                    cmd = "./load_data --host " + host + " --username Administrator --password password "
+                    cmd += " --batches " + str(cv["batches"]) + " --batch-size " + str(cv["batchsize"])
+                    cmd += " --bucket " + b + " --scope " + sv["name"] + " --collection " + cv["name"]
+                    systemcmd(cmd)
+                    if cfg["usebackup"] and bv["type"] != "free" :
+                        backupcollection = b + "." + sv["name"] + "." + cv["name"]
+                        systemcmd("/bin/rm -rf " + cfg["backupdir"])
+                        cmd = "/opt/couchbase/bin/cbbackupmgr config --archive " + cfg["backupdir"]
+                        cmd += " --repo default --disable-bucket-config --include-data " + backupcollection
+                        systemcmd(cmd)
+                        cmd = "/opt/couchbase/bin/cbbackupmgr backup --archive " + cfg["backupdir"]
+                        cmd += " --repo default --cluster " + cfg["host"]
+                        cmd += " --username Administrator --password password --threads 16 "
+                        systemcmd(cmd)
+                else :
+                     cmd = "/opt/couchbase/bin/cbbackupmgr restore --archive " + cfg["backupdir"]
+                     cmd += " --repo default --cluster " + cfg["host"]
+                     cmd += " --username Administrator --password password --threads 16 --purge "
+                     cmd += " --map-data " + backupcollection +"=" + b + "." + sv["name"] + "." + cv["name"]
+                     systemcmd(cmd)
                 create_collection_indexes(conn, f, sv["collections"][cc])
-
     f.close()
+    wait_build_indexes(conn)
         
 def create_javascript_udf(tenant) :
     cmd = "curl -s -k -X POST "
@@ -262,6 +283,36 @@ def prepare_stmts(conn, workload) :
                                         "stmt":lstmt.replace("col",qc+".col"), "qtype": k})
         bv["prepareds"] = sqs
     return
+
+def wait_build_indexes(conn) :
+    cnt = 1
+    while cnt > 0 :
+        cbody = n1ql_execute(conn, {"statement": "SELECT RAW COUNT(1) FROM system:indexes AS s WHERE s.state = 'deferred'"} , None)
+        cnt = cbody["results"][0]
+        if cnt == 0 :
+            break
+        bstmt = {"statement":"SELECT k, inames FROM system:indexes AS s LET k = NVL2(s.bucket_id, CONCAT2('.', s.bucket_id, s.scope_id, s.keyspace_id), s.keyspace_id) WHERE s.namespace_id = 'default' GROUP BY k LETTING inames = ARRAY_AGG(s.name) FILTER (WHERE s.state = 'deferred') HAVING ARRAY_LENGTH(inames) > 0"}
+        body = n1ql_execute(conn, bstmt , None)
+        for i in body["results"] :
+            r = body["results"][i]
+            bindexes = ""
+            for k in r["inames"] :
+                 if k != 0 :
+                     bindexes += ","
+                 bindexes += r["inames"][k]
+            print ("BUILD INDEX ON ", k, "(", bindexes, ")")
+            n1ql_execute(conn, {"statement": "BUILD INDEX ON " + r["k"] + " (" + bindexes + ")"}, None)
+            time.sleep(5)
+        time.sleep(5)
+
+    cnt = 1
+    while cnt > 0 :
+        cbody = n1ql_execute(conn, {"statement": "SELECT RAW COUNT(1) FROM system:indexes AS s WHERE s.state != 'online'"} , None)
+        cnt = cbody["results"][0]
+        if cnt == 0 :
+            break
+        print ("WAITING FOR INDEXES READY : ", cnt)
+        time.sleep(5)
 
 def tenant_distribution(nthreads, workload):
     tenants = []
@@ -396,15 +447,23 @@ def result_finish(wfd, results) :
          if fbresult[b]["count"] != 0:
             fbresult[b]["avg"] = round((fbresult[b]["time"]/fbresult[b]["count"])*1000,3)
          fbresult[b]["time"] = round(fbresult[b]["time"]*1000,3)
+         fbresult[b]["qps"] = round(fbresult[b]["count"]*1000/fbresult[b]["time"], 3)
+         fbresult[b]["qpspc"] = round(fbresult[b]["qps"]/cfg["ncores"], 3)
 
     count = 0
     total = 0.0
+    qps = 0.0
+    qpspc = 0.0
     for q in sorted(fqresult.keys()) :
          if fqresult[q]["count"] != 0:
             fqresult[q]["avg"] = round((fqresult[q]["time"]/fqresult[q]["count"])*1000,3)
             count += fqresult[q]["count"]
             total += fqresult[q]["time"]
          fqresult[q]["time"] = round(fqresult[q]["time"]*1000,3)
+         fqresult[q]["qps"] = round(fqresult[q]["count"]*1000/fqresult[q]["time"], 3)
+         fqresult[q]["qpspc"] = round(fqresult[q]["qps"]/cfg["ncores"], 3)
+         qps += fqresult[q]["qps"]
+         qpspc += fqresult[q]["qpspc"]
     
     wfd.write("\n\n ---------------BEGIN REQUESTS BY TENANT (ms)-----------\n")
     for b in sorted(fbresult.keys()) :
@@ -415,7 +474,7 @@ def result_finish(wfd, results) :
     for q in sorted(fqresult.keys()) :
         wfd.write("    " + q.upper() + " " + json.dumps(fqresult[q]) + "\n")
     wfd.write("\n ---------------END REQUESTS BY QUERY-----------\n")
-    wfd.write("\n    TOTAL REQUESTS : " + str(count) +  "  TOTAL TIME(ms) : " + str(round(total*1000,3)) + " AVG TIME(ms) : " + str(round((total/count)*1000,3)) + "\n\n")
+    wfd.write("\n    TOTAL REQUESTS : " + str(count) +  "  TOTAL TIME(ms) : " + str(round(total*1000,3)) + " AVG TIME(ms) : " + str(round((total/count)*1000,3)) +  " QPS : " + str(qps) + " QPSPC : " + str(qpspc) + "\n\n")
     
 def run_execute(conn, wfd, workload) :
     if not cfg["execute"]:
